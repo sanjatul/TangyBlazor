799b8b89a40eaba90261d3d794df54eb12bb1b7d
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-09-21 12:12:45 +0600

    Genralized Toaster integration

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToaster = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, 'Operation successful', { timeOut: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, 'Operation failed');
+    }
+}
